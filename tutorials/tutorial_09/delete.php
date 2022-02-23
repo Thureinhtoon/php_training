e1866888be4ff702cc242e9bc188d04c69844aa9
=ko thu <=thureinhtun2499@gmail.com>
2022-02-23 15:34:31 +0630

    [master]-tutorial09 reviews

--- tutorials/tutorial_09/delete.php
+++ tutorials/tutorial_09/delete.php
@@ -18,7 +18,7 @@ mysqli_close($connect);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>delete person | tutorial 08</title>
+    <title>delete person | tutorial 09</title>
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
     <script type="text/javascript" src="jquery/jquery.min.js"></script>
     <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
--- tutorials/tutorial_09/index.php
+++ tutorials/tutorial_09/index.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>tutorial 08</title>
-    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
-    <script type="text/javascript" src="jquery/jquery.min.js"></script>
-    <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
-</head>
-
-<body>
-    <div class="container">
-        <h3>Tutorial 08</h3>
-        <a href="create.php" class=" btn btn-primary my-5">Create User</a>
-        <?php
+<?php
 include_once 'connect.php';
 
 function showMessage($input, $message)
@@ -29,14 +12,34 @@ function showMessage($input, $message)
               </div>";
     }
 }
-showMessage("delete", "Your record  deleted successfully");
-showMessage("updated", "Your record  updated successfully");
-showMessage("created", "Your record  created successfully");
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-$select = 'SELECT * from users;';
-$execute = mysqli_query($connect, $select);
-if (mysqli_num_rows($execute) > 0) {
-    echo "<table  class='table'>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tutorial 09</title>
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+    <script type="text/javascript" src="jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <h3>Tutorial 08</h3>
+        <a href="create.php" class=" btn btn-primary my-5">Create User</a>
+        <?php
+
+        showMessage("delete", "Your record  deleted successfully");
+        showMessage("updated", "Your record  updated successfully");
+        showMessage("created", "Your record  created successfully");
+
+        $select = 'SELECT * from users;';
+        $execute = mysqli_query($connect, $select);
+        if (mysqli_num_rows($execute) > 0) {
+            echo "<table  class='table'>
                 <thead class='thead-dark'>
                     <tr>
                         <th scope='col'>Id</th>
@@ -47,36 +50,31 @@ if (mysqli_num_rows($execute) > 0) {
                         <th scope='col'>Actions</th>
                     </tr>
                 </thead>";
-    while ($row = mysqli_fetch_assoc($execute)) {
-        echo "<tr>";
-        echo "<td>" . $row['id'] . "</td>";
-        echo "<td>" . $row['name'] . "</td>";
-        echo "<td>" . $row['email'] . "</td>";
-        echo "<td>" . $row['phone_number'] . "</td>";
-        echo "<td class='city'>" . $row['city'] . "</td>";
-        echo "<td>
+            while ($row = mysqli_fetch_assoc($execute)) {
+                echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>";
+                echo "<td>" . $row['name'] . "</td>";
+                echo "<td>" . $row['email'] . "</td>";
+                echo "<td>" . $row['phone_number'] . "</td>";
+                echo "<td class='city'>" . $row['city'] . "</td>";
+                echo "<td>
                 <a href=\"update.php?id={$row['id']}\" class='btn btn-sm btn-success'>edit</a> |
                 <a href=\"delete.php?id={$row['id']}\" class='btn btn-sm btn-danger'>delete</a>
               </td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-} else {
-    echo "users table have no record yet.";
-}
-mysqli_close($connect);
-?>
+                echo "</tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "users table have no record yet.";
+        }
+        mysqli_close($connect);
+        ?>
 
         <div class="chart">
             <canvas id="myChart"></canvas>
         </div>
-        <!--<script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js"></script>-->
         <script src="node_modules/chart.js/dist/chart.js"></script>
-        <script src="./script.js"></script>
-
-
-
+        <script src="./js/script.js"></script>
     </div>
 </body>
-
 </html>
